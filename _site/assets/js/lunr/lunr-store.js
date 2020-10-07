bf0b4f02a605a9b1bc0a7818b8076826fe35ba04
hg <hynugyeom.kim@teamsquare.com>
2020-10-07 21:49:10 +0900

    []

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Netflix feign logging tip",
+        "excerpt":"TBD  ","categories": ["feign"],
+        "tags": ["feign","logging"],
+        "url": "http://localhost:4000/feign/aaa/",
+        "teaser": null
+      }]
